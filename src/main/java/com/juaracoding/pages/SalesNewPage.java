edc6fc47e9771da88efc8b0a083659960a3b2bad
astrin <astrinindahh@gmail.com>
2024-01-18 18:57:19 +0700

    Update New 4 pada page

--- src/main/java/com/juaracoding/pages/SalesNewPage.java
+++ src/main/java/com/juaracoding/pages/SalesNewPage.java
@@ -2,6 +2,7 @@ package com.juaracoding.pages;
 
 
 import com.juaracoding.drivers.DriverSingleton;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,49 +17,113 @@ public class SalesNewPage {
         PageFactory.initElements(driver, this);
     }
     @FindBy(xpath="//*[@id=\"sidebar\"]/div/div[1]/ul[2]/li[4]/a")
-    private WebElement newMenu;
+    WebElement newMenu;
 
     @FindBy(xpath="//input[@id='tgl']")
-    private WebElement startDate;
+    WebElement startDate;
 
     @FindBy(xpath="//input[@id='tgl2']")
-    private WebElement endDate;
+    WebElement endDate;
 
     @FindBy(xpath="//button[@id='btn-filter']")
-    private WebElement btnFilterData;
+    WebElement btnFilterData;
 
     @FindBy(xpath="//button[@id='btn-reset']")
-    private WebElement btnResetDate;
+    WebElement btnResetDate;
 
     @FindBy(xpath="//input[@type='search']")
-    private WebElement searchKeywordInput;
+    WebElement searchKeywordInput;
 
     @FindBy(xpath="//i[@class='fa fa-search']")
-    private WebElement btnSearchIcon;
+    WebElement btnSearchIcon;
 
     @FindBy(xpath= "//span[@id='btnRefresh']")
-    private WebElement btnRefreshData;
+    WebElement btnRefreshData;
 
     @FindBy(xpath= "//tbody/tr[1]/td[7]/center[1]/a[1]/span[1]")
-    private WebElement btnViewData ;
+    WebElement btnViewData ;
 
 
     @FindBy(xpath= "//button[@id='btnEdit']")
-    private WebElement btnEditData;
+    WebElement btnEditData;
 
     @FindBy(xpath= "//textarea[@id='address']")
-    private WebElement updateAlamat;
+    WebElement updateAlamat;
 
     @FindBy(xpath= "//button[@id='btnUpdate']")
-    private WebElement btnSaveData;
+    WebElement btnSaveData;
 
     @FindBy(xpath= "//button[@id='btnCancelUpdate']")
-    private WebElement btnCancelEdit;
+    WebElement btnCancelEdit;
 
     @FindBy(xpath= "//tbody/tr[1]/td[3]/div[1]/a[2]/i[1]")
-    private WebElement btnIconDeleteUpload;
+    WebElement btnIconDeleteUpload;
 
     @FindBy(xpath= "//a[@class='btn btn-primary']")
-    private WebElement btnSubmit;
+    WebElement btnSubmit;
 
+
+    public void clickNavMenu(){
+        this.newMenu.click();
+    }
+
+    public void clickStartDate(){
+        this.startDate.click();
+    }
+
+    public void clickStartDate(String startDateFilter){
+        this.startDate.sendKeys(startDateFilter);
+    }
+
+    public void clickEndDate(){
+        this.endDate.click();
+    }
+
+    public void clickEndDate(String endDateFilter){
+        this.endDate.sendKeys(endDateFilter);
+    }
+
+    public void clickBtnFilterData() {
+        this.btnFilterData.click();
+
+    }
+
+    public void clickBtnResetDate(){
+        this.btnResetDate.click();
+    }
+
+    public void setInputSearchKeyword(String searchKeywordInput){
+        this.searchKeywordInput.sendKeys(Keys.CONTROL + "A" + Keys.DELETE);
+        this.searchKeywordInput.sendKeys(searchKeywordInput);
+    }
+
+    public void clickBtnRefreshData(){
+        this.btnRefreshData.click();
+    }
+
+    public void clickBtnViewData(){
+        this.btnViewData.click();
+    }
+
+    public void clickBtnEditData(){
+        this.btnEditData.click();
+    }
+
+    public void setInputUpdateAlamat(String updateAlamat){
+    this.updateAlamat.sendKeys(Keys.CONTROL + "A" + Keys.DELETE);
+    this.updateAlamat.sendKeys(updateAlamat);
+    }
+    public void clickBtnSaveData(){
+        this.btnSaveData.click();
+    }
+    public void clickbtnCancelEdit(){
+        this.btnCancelEdit.click();
+    }
+    public void clickBtnIconDeleteUpload(){
+        this.btnIconDeleteUpload.click();
+    }
+
+    public void clickBtnSubmit(){
+        this.btnSubmit.click();
+    }
 }
