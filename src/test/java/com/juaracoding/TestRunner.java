0f2891a902950c4807384ea6b6cb892080cd52dc
ikhsanmajid <ikhsanmajid@gmail.com>
2024-01-19 05:45:22 +0700

    1

--- src/test/java/com/juaracoding/TestRunner.java
+++ src/test/java/com/juaracoding/TestRunner.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        //features = {"src/main/resources/features/1_Login_Sales.feature", "src/main/resources/features/2_Sales_Input_Data.feature", "src/main/resources/features/3_Sales_Upload_Dokumen.feature", "src/main/resources/features/4_Sales_Tanda_Tangan.feature"},
-        features = {"src/main/resources/features/6_Login_Admin.feature", "src/main/resources/features/7_Admin_ViewExport.feature"},
+        features = {"src/main/resources/features/1_Login_Sales.feature", "src/main/resources/features/2_Sales_Input_Data.feature", "src/main/resources/features/3_Sales_Upload_Dokumen.feature", "src/main/resources/features/4_Sales_Tanda_Tangan.feature"},
+//        features = {"src/main/resources/features/6_Login_Admin.feature", "src/main/resources/features/7_Admin_ViewExport.feature"},
         glue = "com.juaracoding",
         plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json"}
 )
